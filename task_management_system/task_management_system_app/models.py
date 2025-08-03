b39e7a7e043a71daca4daac6860002cc7b107189
Tasfiq-K <tasfiq.kamran@gmail.com>
2025-08-03 10:51:13 +0600

    --added model for comments

--- task_management_system/task_management_system_app/models.py
+++ task_management_system/task_management_system_app/models.py
@@ -7,6 +7,12 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
@@ -24,6 +30,24 @@ class Task(models.Model):
     description = models.TextField(default='')
     location = models.CharField(max_length=255, default='')
     organizer = models.CharField(max_length=100, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.name} ({self.category.name})"
+    
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    commented_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.task.name}"
+    
+    class Meta:
+        ordering = ['-commented_at']
 
 
     
