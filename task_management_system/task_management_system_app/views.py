26571e002ce7f73b450a10643fa31f5d2bcf9277
Tasfiq-K <tasfiq.kamran@gmail.com>
2025-08-03 17:17:53 +0600

    --added server side validation for start date and end date in TaskForm class

--- task_management_system/task_management_system_app/views.py
+++ task_management_system/task_management_system_app/views.py
@@ -65,6 +65,15 @@ class TaskForm(forms.ModelForm):
         if not user.is_superuser:
             self.fields['assigned_to'].queryset = User.objects.filter(id=user.id)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get('start_date')
+        end_date = cleaned_data.get('end_date')
+        if start_date and end_date and end_date <= start_date:
+            self.add_error('end_date', 'End date must be after start date')
+        return cleaned_data
+
+
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
