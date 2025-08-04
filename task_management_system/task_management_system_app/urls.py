87554aca3feaacfc04196f05efd9301585c1703f
Tasfiq-K <tasfiq.kamran@gmail.com>
2025-08-04 11:08:35 +0600

    --login page is the landing page

--- task_management_system/task_management_system_app/urls.py
+++ task_management_system/task_management_system_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'task_management_system_app'
 
 urlpatterns = [
+    path('', views.user_login, name='login'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
@@ -11,7 +12,7 @@ urlpatterns = [
     path('tasks/create/', views.create_task, name='create_task'),
     path('tasks/update/<int:task_id>/', views.update_task, name='update_task'),
     path('tasks/delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('', views.category_list, name='category_list'),
+    path('categories/', views.category_list, name='category_list'),
     path('categories/create/', views.create_category, name='create_category'),
     path('categories/<int:category_id>/', views.category_tasks, name='category_tasks'),
     path('categories/delete/<int:category_id>/', views.delete_category, name='delete_category'),
